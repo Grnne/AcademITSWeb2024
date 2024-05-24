04d351babce36213244c5b068787d69299888ce5
Serge Zakhary <grimglance@gmail.com>
2024-05-24 18:03:10 +0700

    Add project files.

---
+++ HtmlTask/Program.cs
@@ -0,0 +1,14 @@
+namespace HtmlTask;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+        app.UseStaticFiles();
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
