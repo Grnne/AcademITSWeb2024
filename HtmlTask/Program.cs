a90e1106d60cfe9abd7866843d3440a3ff552c88
Serge Zakhary <grimglance@gmail.com>
2024-05-24 23:58:11 +0700

    1

--- HtmlTask/Program.cs
+++ HtmlTask/Program.cs
@@ -7,7 +7,7 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
         app.UseStaticFiles();
-        app.MapGet("/", () => "Hello World!");
+        //app.MapGet("/", () => "Hello World!");
 
         app.Run();
     }
