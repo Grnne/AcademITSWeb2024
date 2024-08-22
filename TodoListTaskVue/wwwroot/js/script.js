48324aeeaa967df3c1d7f485aee7f3d4f78522be
Serge Zakhary <grimglance@gmail.com>
2024-08-22 18:42:10 +0700

    added vue task list

--- TodoListTaskVue/wwwroot/js/script.js
+++ TodoListTaskVue/wwwroot/js/script.js
@@ -13,41 +13,67 @@ Vue.createApp({})
         methods: {
             addTodoItem() {
                 const newTodoItem = {
-                    id: this.newTodoItemId,
-                    text: this.newTodoItemText
+                    id: this.newTodoItemId++,
+                    text: this.newTodoItemText.trim()
                 };
 
-                this.newTodoItemId++;
-                this.items.push(newTodoItem);
-                this.newTodoItemText = "";
+                if (newTodoItem.text) {
+                    this.items.push(newTodoItem);
+                    this.newTodoItemText = "";
+                }
             },
             deleteTodoItem(item) {
-                this.items = this.items.filter(x => x !== item);
+                this.items = this.items.filter(x => x.id !== item.id);
             },
+            handleSubmit(event) {
+                event.preventDefault();
+                const form = event.target;
+
+                if (!form.checkValidity()) {
+                    form.classList.add("was-validated");
+                    return;
+                }
+
+                this.addTodoItem();
+                form.classList.remove("was-validated");
+            }
         },
 
         template: `
-        <form @submit.prevent = "addTodoItem" class= "row mb-3" >
-            <label class="col">
-                <input v-model="newTodoItemText" class="form-control" type="text" placeholder="Input your task">
-            </label>
-            <div class="col-auto">
-                <button class="btn btn-primary">Add</button>
-            </div>
-        </form >
-        <ul class="list-unstyled">
-            <todo-list-item v-for="item in items"
+        <div class="container-flex m-4">
+            <div class="row justify-content-center">
+                <div class="col-md-7">
+                    <form @submit="handleSubmit" class="needs-validation" novalidate>
+                        <div class="form-group">
+                            <input v-model="newTodoItemText"
+                                class="form-control"
+                                type="text"
+                                placeholder="Input your task"
+                                required>
+                            <div class="invalid-feedback">Specify a task.</div>
+                        </div>
+                        <div class="text-center mt-2">
+                            <button class="btn btn-primary btn-lg m-2">Add</button>
+                        </div>
+                    </form>
+                    <ul class="list-unstyled m-5">
+                        <todo-list-item 
+                            v-for="item in items"
                             :key="item.id"
                             :item="item"
                             @save-item="item.text = $event"
-                            @delete-item="deleteTodoItem(item)"></todo-list-item>
-        </ul > `
+                            @delete-item="deleteTodoItem(item)">
+                        </todo-list-item>
+                    </ul>
+                </div>
+            </div>
+        </div>`
     })
     .component("TodoListItem", {
         props: {
             item: {
                 type: Object,
-                requred: true
+                required: true
             }
         },
 
@@ -60,34 +86,48 @@ Vue.createApp({})
         methods: {
             save() {
                 this.isEditing = false;
-                this.$emit("save-item", this.editingText);
+                this.$emit("save-item", this.editingText.trim());
             },
             cancel() {
                 this.isEditing = false;
                 this.editingText = this.item.text;
+            },
+            handleSubmit(event) {
+                event.preventDefault();
+                const form = event.target;
+
+                if (!form.checkValidity()) {
+                    form.classList.add("was-validated");
+                    return;
+                }
+
+                this.save();
+                form.classList.remove("was-validated");
             }
         },
 
         template: `
-        < li class= "mb-2" >
-        <div class="row" v-if="!isEditing">
-            <div class="col">
-                <span class="me-1 form-control">{{ item.text }}</span>
+        <li class="list-group mb-2">
+            <div class="row" v-if="!isEditing">
+                <div class="col list-group-item overflow-auto">
+                    <span class="me-1 overflow-scroll">{{ item.text }}</span>
+                </div>
+                <div class="col-auto">
+                    <button @click="$emit('delete-item')" class="btn btn-danger me-1" type="button">Delete</button>
+                    <button @click="isEditing = true" class="btn btn-primary" type="button">Edit</button>
+                </div>
             </div>
-            <div class="col-auto">
-                <button @click="$emit('delete-item')" class="btn btn-danger me-1" type="button">Delete</button>
-            <button @click="isEditing = true" class="btn btn-primary" type="button">Edit</button>
-            </div >
-        </div >
-        <div class="row" v-else>
-            <div class="col">
-                <input v-model="editingText" class="me-1 form-control" type="text">
-            </div>
-            <div class="col-auto">
-                <button @click="cancel" class="btn btn-primary me-1" type="button">Cancel</button>
-            <button @click="save" class="btn btn-primary" type="button">Save</button>
-             </div >
-             </div >
-        </li > `
-    })
-    .mount("#app");
+            <form @submit="handleSubmit" class="needs-validation" novalidate v-else>
+                <div class="row">
+                    <div class="form-group col">
+                        <input v-model="editingText" class="me-1 form-control" type="text" required>
+                        <div class="invalid-feedback">Specify a task.</div>
+                    </div>
+                    <div class="col-auto">
+                        <button @click="cancel" class="btn btn-secondary me-1" type="button">Cancel</button>
+                        <button type="submit" class="btn btn-primary">Save</button>
+                    </div>
+                </div>
+            </form>
+        </li>`
+    }).mount("#app");
